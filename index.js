b4983c6fb985d7bd52ae11b76e8bbb0a399b1d8d
Charandeep Kumar <96859500+charandeep7@users.noreply.github.com>
2024-02-29 15:55:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const { spawn } = require("child_process");
+const { random } = require('glowing-engine');
+const readline = require("readline");
+const fs = require("fs");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function downloadVideo(url, outputName, outputPath) {
+  const ffmpegCommand = `ffmpeg -i "${url}" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 "${outputPath}/${outputName}.mp4"`;
+
+  const childProcess = spawn(ffmpegCommand, {
+    shell: true,
+    detached: true, // Detach the child process from the parent
+    stdio: "ignore" // Ignore the standard I/O streams
+  });
+
+  childProcess.unref(); // Unreference the child process to allow the parent to exit independently
+}
+
+function promptUser() {
+  rl.question("Enter the m3u8 URL: ", (url) => {
+    rl.question("Enter the output file name (press Enter for default): ", (outputName) => {
+        const defaultName = (new Date()).toString().concat('download').split(' ').slice(2,4).join('').concat(random.randomNumberInRange(1,9999999999999).toString());
+        outputName = outputName || defaultName;
+      rl.question("Enter the output folder path (press Enter for current directory): ", (outputPath) => {
+        const defaultOutputPath = process.cwd(); 
+        outputPath = outputPath || defaultOutputPath; 
+        if (!fs.existsSync(outputPath)) {
+          console.error(`Error: Output folder '${outputPath}' does not exist.`);
+          rl.close();
+          return;
+        }
+        downloadVideo(url, outputName, outputPath);
+        rl.close();
+      });
+    });
+  });
+}
+
+promptUser();
